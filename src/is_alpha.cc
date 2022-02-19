3938a6cadd50ec5e9dbde9a90b5513836f87e0ef
nivethan <you@example.com>
2022-02-19 03:06:07 -0500

    working away

--- src/is_alpha.cc
+++ src/is_alpha.cc
@@ -6,23 +6,27 @@
 #include "universe.h"
 
 Napi::Value Universe::IsAlpha(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+
     if (this->_session_id == 0) {
-        Napi::Env env = info.Env();
-        char error[100];
-        snprintf(error, 100, "Session has not been started.\n");
-        Napi::TypeError::New(env, error).ThrowAsJavaScriptException();
+        Napi::TypeError::New(env, "Session has not been started.").ThrowAsJavaScriptException();
         return env.Null();
     }
 
     std::string temp = info[0].ToString().Utf8Value();
     std::string param = UTF8toISO8859_1(temp.c_str());
 
-    const char *query = param.c_str();
-    long query_len = strlen(query);
+    long query_len = param.length();
 
     long code;
-    ic_alpha((char *)query, &query_len, &code);
+    ic_alpha(param.data(), &query_len, &code);
 
-    Napi::Env env = info.Env();
+    if (code > 1) {
+        char error[100];
+        snprintf(error, 100, "Error in checking alpha. Code: %ld", code);
+        Napi::TypeError::New(env, error).ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
     return Napi::Number::New(env, code);
 }
